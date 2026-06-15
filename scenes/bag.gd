073278125382d20c4da9365dae5ddf4177f91b38
kyouki777 <emilyjo333@gmail.com>
2026-06-15 15:48:07 +0800

    day 1
    core gameplay

--- scenes/bag.gd
+++ scenes/bag.gd
@@ -29,13 +29,55 @@ func buy_cursor2():
 
 	if main.clicks >= cost:
 		main.clicks -= cost
-		main.amount_per_click += 2
+		main.amount_per_click += 3
+
+		main.emit_signal("clicks_changed", main.clicks)
+		main.save_data()
+		
+func buy_cursor3():
+	var main = get_tree().current_scene
+	var cost = 10000
+
+	if main.clicks >= cost:
+		main.clicks -= cost
+		main.amount_per_click += 150
+
+		main.emit_signal("clicks_changed", main.clicks)
+		main.save_data()
+
+func buy_cursor4():
+	var main = get_tree().current_scene
+	var cost = 100000
+
+	if main.clicks >= cost:
+		main.clicks -= cost
+		main.amount_per_click += 1500
+
+		main.emit_signal("clicks_changed", main.clicks)
+		main.save_data()
+		
+func buy_cursor5():
+	var main = get_tree().current_scene
+	var cost = 1000000
+
+	if main.clicks >= cost:
+		main.clicks -= cost
+		main.amount_per_click += 15000
 
 		main.emit_signal("clicks_changed", main.clicks)
 		main.save_data()
 
 func _on_extra_click_button_down():
 	buy_cursor()
-	
+
 func _on_extra_click_2_button_down() -> void:
 	buy_cursor2()
+
+func _on_extra_click_3_button_down() -> void:
+	buy_cursor3()
+
+func _on_extra_click_4_button_down() -> void:
+	buy_cursor4()
+
+func _on_extra_click_5_button_down() -> void:
+	buy_cursor5()
